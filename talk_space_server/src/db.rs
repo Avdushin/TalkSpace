f511767b70cffb82268e7609f5993dab2aafad12
Avdushin <avdushinbeaver1@gmail.com>
2024-10-09 17:47:03 +0300

    Some opts

--- talk_space_server/src/db.rs
+++ talk_space_server/src/db.rs
@@ -4,13 +4,11 @@ use crate::config::get_database_url;
 pub async fn init_db() -> PgPool {
     let database_url = get_database_url();
 
-    // Подключение к базе данных
     let pool = PgPoolOptions::new()
         .connect(&database_url)
         .await
         .expect("Failed to create pool.");
 
-    // Выполним миграции (создание таблиц, если их нет)
     sqlx::migrate!("./migrations")
         .run(&pool)
         .await
